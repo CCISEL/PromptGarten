6ef33ab7057c6b34a891d0e8671e365233e23255
Duarte Nunes <duartenunes@cc.isel.ipl.pt>
2011-03-10 20:17:56 +0000

    added a generic repository

---
+++ src/Main/PromptGarten.Domain/Services/Domain/IRepository.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Linq;
+using System.Linq.Expressions;
+
+namespace PromptGarten.Domain.Services
+{
+    public interface IRepository
+    {
+        IQueryable<TEntity> Query<TEntity>();
+
+        void Save<TEntity>(TEntity target);
+        void Delete<TEntity>(TEntity target);
+    }
+
+    public static class RepositoryExtensions
+    {
+        public static TEntity Find<TEntity>(this IRepository repository, Expression<Func<TEntity, bool>> filter)
+        {
+            return repository.Query<TEntity>().SingleOrDefault(filter);
+        }
+    }
+}
