fe42d609457ede140da154da5e25e0571815d57a
Duarte Nunes <duarte.m.nunes@gmail.com>
2011-03-11 17:46:34 +0000

    added an in memory repository

---
+++ src/PromptGarten.Common/DictionaryExtensions.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace PromptGarten.Common
+{
+    public static class DictionaryExtensions
+    {
+        public static TValue FindOrCreate<TKey, TValue>(this IDictionary<TKey, TValue> items, TKey key, Func<TValue> value)
+        {
+            TValue res;
+            if (!items.TryGetValue(key, out res))
+            {
+                res = value();
+                items.Add(key, res);
+            }
+            return res;
+        }
+    }
+}
---
+++ src/Test/PromptGarten.Domain.Test/InMemoryRepository.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using PromptGarten.Common;
+using PromptGarten.Domain.Services;
+
+namespace PromptGarten.Domain.Test
+{
+    public class InMemoryRepository : IRepository, IEnumerable
+    {
+        private readonly IDictionary<Type, List<object>> _db = new Dictionary<Type, List<object>>();
+
+        public void Add<TAggregate>(TAggregate aggregate)
+        {
+            _db.FindOrCreate(typeof(TAggregate), () => new List<object>()).Add(aggregate);
+        }
+
+        public IQueryable<TAggregate> Query<TAggregate>()
+        {
+            return _db[typeof(TAggregate)].Cast<TAggregate>().AsQueryable();
+        }
+
+        public void Save<TAggregate>(TAggregate target)
+        {
+            Add(target);
+        }
+
+        public void Delete<TAggregate>(TAggregate target)
+        {
+            if (_db[typeof(TAggregate)].Remove(target) == false)
+            {
+                throw new InvalidOperationException();
+            }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return _db.GetEnumerator();
+        }
+    }
+}
