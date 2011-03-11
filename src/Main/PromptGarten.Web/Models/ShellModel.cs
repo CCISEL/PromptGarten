35bfd8c5caf2652deb9b27638f0b0a015ab65634
Pedro Felix <pmhsfelix@gmail.com>
2011-03-11 00:39:42 +0000

    added shell

---
+++ src/Main/PromptGarten.Web/Models/ShellModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace PromptGarten.Web.Models
+{
+    public class ShellModel
+    {
+        public string Command { get; set; }
+        public string Output { get; set; }
+    }
+}
